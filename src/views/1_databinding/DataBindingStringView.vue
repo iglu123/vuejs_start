5d632a762e0a9224efe9f882507e62d3afc707a3
JSJ <seongjoon.jeong@dkbmc.com>
2022-11-25 16:58:32 +0900

    vue.js starting//databindingstring

---
+++ src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Hello {{ userName }}</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: 'John Doe',
+      message: 'Welcome 환영~',
+      arr: [],
+      obj: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-red {
+  color: red;
+}
+</style>
